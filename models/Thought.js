71d49fadd9f3a0630294f24bd4a4bf6c4114c563
David Luu <dluu.audeo@gmail.com>
2022-02-23 19:44:16 -0800

    updated models

---
+++ models/Thought.js
@@ -0,0 +1,27 @@
+const { Schema, Types } = require('mongoose');
+const userSchema = require('./User');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: {
+            validator: function(email) {
+                return  /^([a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})$/.test(email);
+            },
+            message: "This is not a valid email."
+        },
+      },
+      thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought' }],
+      friends: [{ type:Schema.Types.ObjectId, ref: 'user' }]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+    }
+);
+  
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return friends.length;
+    })
+
+
+const User = model('user', userSchema);  
+module.exports = User;
+  
