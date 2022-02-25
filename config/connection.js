026a350a5a92641b33c4650738553a4efcd44bb4
David Luu <dluu.audeo@gmail.com>
2022-02-25 01:10:06 -0800

    revised connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/randomThoughts', {
+connect('mongodb://localhost:27017/randomThoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
